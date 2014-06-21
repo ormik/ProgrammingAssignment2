a37fc49428f9d96d3b0b805558b88e84ec698847
ormik <orm1@interia.pl>
2014-06-21 16:35:12 +0200

    1st version
    Not tested version

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,23 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  InvMatrix<-NULL
+  
+  SetMatrix<-function(y)
+  {
+    InvMatrix<-NULL
+    x<<-y
+  }
+  
+  GetMatrix<-function() x
+  
+  SetInvMatrix<-function(im) InvMatrix<<-im
+  
+  GetInvMatrix<-function() InvMatrix
+  
+  list(SetMatrix=SetMatrix, GetMatrix= GetMatrix,
+       SetInvMatrix=SetInvMatrix,GetInvMatrix=GetInvMatrix)
 }
 
 
@@ -12,4 +28,17 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  InvMatrix<-x$GetInvMatrix()
+  if(!is.null(InvMatrix))
+  {
+    message("getting cached data")
+  }
+  else
+  {
+    message("no cached data")
+    InvMatrix<-solve(x$GetMatrix())
+    x$SetMatrix(InvMatrix)
+  }
+
+  InvMatrix
 }
